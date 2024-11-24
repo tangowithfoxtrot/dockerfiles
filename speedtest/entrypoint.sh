b2b7db748f1ccb0f4bfaa465f291d6deadc92419
tangowithfoxtrot <5676771+tangowithfoxtrot@users.noreply.github.com>
2024-11-24 10:41:23 -0800

    chore(speedtest): add human-readable format

--- speedtest/entrypoint.sh
+++ speedtest/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-FORMAT=${FORMAT:-json} # csv, tsv, json, jsonl, json-pretty
-UNIT=${UNIT:-Mbps} # Kbps, Mbps, Gbps
+FORMAT=${FORMAT:-json} # csv, tsv, json, jsonl, json-pretty, human-readable
+UNIT=${UNIT:-Mbps}     # Kbps, Mbps, Gbps
 speedtest --accept-license --accept-gdpr --format="$FORMAT" --unit="$UNIT" 2>/dev/null
