02162a073737d46b1207d6b3603bb9831d286e2c
tangowithfoxtrot <5676771+tangowithfoxtrot@users.noreply.github.com>
2024-11-24 10:53:29 -0800

    ci: add build script

---
+++ build-images.sh
@@ -0,0 +1,411 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2155
+set -eo pipefail
+
+# env setup
+find_contexts() {
+  # find all directories containing a Dockerfile
+  find . -type f -name Dockerfile -exec dirname {} \; | sort | sed 's|^\./||'
+}
+
+# all vars in this section must be duplicated in the do_in_docker function
+BASENAME="$(basename "$0")"                                                                # name of the script
+BUILD_PLATFORMS="${BUILD_PLATFORMS:-linux/amd64,linux/arm64}"                              # platforms to build for
+CURL_ARGS="${CURL_ARGS:-"--header \"Authorization: Bearer $(gh auth token || echo -n)\""}" # curl arguments; try to use a GitHub token if available
+CONTAINER_BUILD_CONTEXTS="${CONTAINER_BUILD_CONTEXTS:-$(find_contexts)}"                   # directories containing Dockerfiles
+CONTAINER_REGISTRY="${CONTAINER_REGISTRY:-docker.io}"                                      # docker.io
+CONTAINER_REPO="${CONTAINER_REPO:-$(git config user.name)}"                                # docker.io/username
+CONTAINER_ENGINE_SOCKET="${CONTAINER_ENGINE_SOCKET:-/var/run/docker.sock}"                 # docker socket; only needed if running in docker
+DOINDOCKER="${DOINDOCKER:-1}"                                                              # run the builds in docker
+DOINDOCKER_BUILD_TOOLS="${DOINDOCKER_BUILD_TOOLS:-bash curl github-cli git jq ncurses}"    # alpine packages needed in the docker container
+REGISTRY_USER="${REGISTRY_USER:-$CONTAINER_REPO}"                                          # username for the registry
+REGISTRY_PAT="${REGISTRY_PAT:-$(gh auth status || echo -n)}"                               # personal access token for the registry
+REPO_ROOT="$(git rev-parse --show-toplevel)"                                               # root of the repo; should never be overridden
+# end of vars that must be duplicated in the do_in_docker function
+
+THIS_VERSION="0.0.1"                                 # version of this script
+export DOCKER_CLI_HINTS="${DOCKER_CLI_HINTS:-false}" # disable ads from the Docker CLI 🖕💩
+
+# utility functions
+
+# mimic the help output from clap-rs
+show_help() {
+  cat <<EOF
+Build script for Docker images
+
+$(tput bold)$(tput smul)Usage:$(tput sgr0) $(tput bold)$0$(tput sgr0) [OPTIONS] [CONTEXT]
+
+$(tput bold)$(tput smul)Context:$(tput sgr0)
+  Any number of Docker build contexts
+  If not provided, all directories containing a Dockerfile will be used
+
+  $(tput bold)$(tput smul)Detected contexts:$(tput sgr0)
+    $(tput bold)$(find_contexts | sed 's|^\./||' | paste -s - | sed 's/\t/\, /g')$(tput sgr0)
+
+$(tput bold)$(tput smul)Options:$(tput sgr0)
+  $(tput bold)-h,     --help$(tput sgr0)    Show this help message
+  $(tput bold)-V,     --version$(tput sgr0) Show the version of this script
+  $(tput bold)-v,     --verbose$(tput sgr0) Enable verbose output
+  $(tput bold)--test, --ttl$(tput sgr0)     Push to the ttl.sh registry
+  $(tput bold)-b,     --build$(tput sgr0)   Build the docker images
+  $(tput bold)-p,     --push$(tput sgr0)    Push the docker images
+  $(tput bold)-u,     --update$(tput sgr0)  Pull the latest version number for the upstream project
+EOF
+}
+
+# used to create a clickable link to the line in this script in log output
+line_output() {
+  echo "$0: line $1"
+}
+
+if [[ -n "${NO_COLOR:-}" ]]; then
+  tput() { :; }
+fi
+
+# enable psuedo breakpoints in debug mode
+if [[ "$BREAKPOINT" == "1" ]]; then
+  # FIXME: TTY requires pressing Enter twice to continue when a function before it redirected stderr...
+  log() {
+    tput bel
+    printf "$(tput setaf 6)❕ %-20s$(tput sgr0)\t\t%s\n" "${context:-main}" "$1"
+    read -r -p "   ↳ $(tput setaf 5)$2:$(tput sgr0) Press Enter to continue." ||
+      { log "entering shell" "$(line_output $LINENO)" && /usr/bin/env bash -i; }
+  } >&2
+  err() {
+    tput bel
+    printf "$(tput setaf 1)❌ %-20s$(tput sgr0)\t\t%s\n\n" "${context:-main}" "$1"
+    read -r -p "   ↳ $(tput setaf 1)$2:$(tput sgr0) Breakpoint. Press Enter to enter an interactive environment, or press Ctrl+C to continue." ||
+      { log "entering shell" "$(line_output $LINENO)" && /usr/bin/env bash -i; }
+  } >&2
+  log "Breakpoints enabled" "$(line_output $LINENO)"
+else
+  log() {
+    printf "$(tput setaf 6)❕ %-20s$(tput sgr0)\t\t%s\n" "${context:-main}" "$1" >&2
+  }
+  err() {
+    printf "$(tput setaf 1)❌ %-20s$(tput sgr0)\t\t%s\n" "${context:-main}" "$1" >&2
+    exit 1
+  }
+fi
+
+# run the builds in a docker container
+# useful to ensure the build environment is consistent
+do_in_docker() {
+  if ! docker ps >/dev/null; then
+    err "Docker is not installed" "$(line_output $LINENO)"
+  fi
+  log "Running in docker" "$(line_output $LINENO)"
+
+  container_id=$(
+    docker run --rm -d -it \
+      -v "$CONTAINER_ENGINE_SOCKET":/var/run/docker.sock \
+      -v "$REPO_ROOT:/$REPO_ROOT" \
+      -w /"$REPO_ROOT" \
+      -e BASENAME="$BASENAME" \
+      -e BREAKPOINT="$BREAKPOINT" \
+      -e BUILD_PLATFORMS="$BUILD_PLATFORMS" \
+      -e CONTAINER_ENGINE_SOCKET=/var/run/docker.sock \
+      -e CONTAINER_BUILD_CONTEXTS="$CONTAINER_BUILD_CONTEXTS" \
+      -e CONTAINER_REGISTRY="$CONTAINER_REGISTRY" \
+      -e CONTAINER_REPO="$CONTAINER_REPO" \
+      -e DOINDOCKER='#already in docker' \
+      -e DOINDOCKER_BUILD_TOOLS="$DOINDOCKER_BUILD_TOOLS" \
+      -e NO_COLOR="$NO_COLOR" \
+      -e REGISTRY_USER="$REGISTRY_USER" \
+      -e REGISTRY_PAT="$REGISTRY_PAT" \
+      -e REPO_ROOT="$REPO_ROOT" \
+      --stop-signal SIGKILL \
+      docker:latest \
+      tail -f /dev/null
+  )
+
+  trap 'docker rm -f "$container_id" >/dev/null || err "Failed to remove container" "$(line_output $LINENO)"' EXIT
+
+  docker exec -it "$container_id" /bin/sh -c "
+    apk update && apk add --no-cache $DOINDOCKER_BUILD_TOOLS
+    ./build-images.sh $* || exit 1
+  "
+}
+
+# get the latest tag from a git repo
+# add special cases for returning the latest tag as needed
+git_latest_tag() {
+  local url="$1"
+
+  case $url in
+  *curl*)
+    # curl releases are tagged with a prefix of "curl-";
+    # we need to grab the latest tag that starts with "curl-"
+    log "curl" "$(line_output $LINENO)"
+    git ls-remote --tags --refs --sort="v:refname" "$url" |
+      awk -F/ '{print $3}' |
+      sort -r |
+      grep -m 1 -e '^curl-'
+    ;;
+  *)
+    git ls-remote --tags --refs --sort="v:refname" "$url" | tail -n1 | awk -F/ '{print $3}'
+    ;;
+  esac
+
+}
+
+# attempt to fetch the latest version from a URL
+fetch_latest_version() {
+  log "fetch_latest_version $1" "$(line_output $LINENO)"
+  local url="$1"
+  if [[ -z "$url" ]]; then
+    log "No URL provided" "$(line_output $LINENO)"
+    echo "NOVER"
+    return 0
+  fi
+
+  # shellcheck disable=SC2086,SC2064
+  # works well for GitHub-hosted code
+  curl -sL \
+    $CURL_ARGS \
+    "$url" | jq -r '.tag_name' 2>/dev/null ||
+    # hopefully works for other git repos
+    { git_latest_tag "$url" 2>/dev/null; } ||
+    { err "could not get latest version." "$(line_output $LINENO)"; }
+}
+
+# retrieve the latest version of an upstream project
+# and store it in a .version file in the context directory
+get_latest_upstream_version() {
+  for context in $CONTAINER_BUILD_CONTEXTS; do
+    local url="$(cat "$context"/.url 2>/dev/null || true)"
+    local ver="$(fetch_latest_version "$url")"
+    if [[ "$ver" == "NOVER" ]]; then
+      log "No VERSION could be derived from $context/.url" "$(line_output $LINENO)"
+      echo "latest" >"$context/.version"
+      continue
+    fi
+
+    log "Latest version of '$context' is '$ver'" "$(line_output $LINENO)"
+    echo "$ver" >"$context/.version"
+  done
+}
+
+# this function will take a variety of version strings, such as any one of the following:
+#   ${NULL}, 1.2.3, v1.2.3, or curl-1_2_3
+# and normalizes them into docker tags, such as:
+#   1.2.3, v1.2.3, curl-1_2_3, latest
+# if we recieve an "odd" version string, like curl-1_2_3,
+# we'll just use that as the tag, along with latest, and the other normalizations
+# if we recieve ${NULL}, skip the normalization and just return "latest"
+create_version_aliases() {
+  local version="$1"
+  local tag_default="$1" # retain the original version string
+  local tag_no_v_prefix
+  local tag_v_prefix
+
+  # exit early if using the default 'latest' version
+  if [[ "$version" == "latest" ]]; then
+    echo "latest"
+    return
+  fi
+
+  # normalize the version string
+  if [[ ! "$version" =~ ^[0-9] ]]; then
+    # log "Replacing non-numeric characters with '.' in $version" "$(line_output $LINENO)"
+    # replace non-numeric characters with '.'
+    version="${version//[^0-9]/.}"
+    # remove any leading or trailing dots
+    version="$(echo "$version" | sed -e 's/^\.*//' -e 's/\.*$//')"
+  fi
+
+  tag_no_v_prefix="$version"
+  tag_v_prefix="v$version"
+
+  echo -e "$version\n$tag_default\n$tag_no_v_prefix\n$tag_v_prefix\nlatest" |
+    sort -u |
+    tr '\n' ' '
+}
+
+build_oci_uri() {
+  local image_name="$1"
+  if [[ "$CONTAINER_REGISTRY" == "ttl.sh" ]]; then
+    log "Using ttl.sh registry" "$(line_output $LINENO)"
+    local version="latest"
+    # local tags="latest"
+  else
+    local tags="$2"
+  fi
+  # local version="$2"
+
+  {
+    # check each component of the URI
+    log "Building docker URI" "$(line_output $LINENO)"
+    log "CONTAINER_REGISTRY: $CONTAINER_REGISTRY" "$(line_output $LINENO)"
+    [[ -n "$CONTAINER_REGISTRY" ]] && echo -n "$CONTAINER_REGISTRY/"
+    log "CONTAINER_REPO: $CONTAINER_REPO" "$(line_output $LINENO)"
+    [[ -n "$CONTAINER_REPO" ]] && echo -n "$CONTAINER_REPO/"
+    log "image_name: $image_name" "$(line_output $LINENO)"
+    [[ -n "$image_name" ]] && echo -n "$image_name:"
+    log "version: $version" "$(line_output $LINENO)"
+    [[ -n "$version" ]] && echo -n "$version"
+  } | tr -d '\n' | sed 's/ $//'
+}
+
+build_docker() {
+  local context="$1"
+  local tag="$2"
+  local push="$3"
+  local version
+
+  log "Building docker images" "$(line_output $LINENO)"
+  log "CONTAINER_BUILD_CONTEXTS: $CONTAINER_BUILD_CONTEXTS" "$(line_output $LINENO)"
+  for context in $CONTAINER_BUILD_CONTEXTS; do
+    version="$(cat "$context/.version" || echo latest)"
+    tags="$(create_version_aliases "$version")"
+
+    log "context: $context" "$(line_output $LINENO)"
+    log "tags: $tags" "$(line_output $LINENO)"
+    log "version: $version" "$(line_output $LINENO)"
+
+    # local oci_uri="$(build_oci_uri "$context" "$version" "$tags")"
+    local oci_uri="$(build_oci_uri "$context" "$tags")"
+    log "Building $oci_uri" "$(line_output $LINENO)"
+    # shellcheck disable=SC2086
+    docker build \
+      --platform "$BUILD_PLATFORMS" \
+      --build-arg VERSION="$version" \
+      -t "$oci_uri" \
+      $push \
+      "$context" || { log "Failed to build $oci_uri. Continuing..." "$(line_output $LINENO)" >/dev/stderr; }
+  done
+
+  for tag in $tags; do
+    new_tag="${oci_uri%:*}:$tag"
+    log "Tagging $oci_uri as $new_tag" "$(line_output $LINENO)"
+    # shellcheck disable=SC2086
+    docker tag "$oci_uri" "$new_tag" || { log "Failed to tag $new_tag. Continuing..." "$(line_output $LINENO)" >/dev/stderr; }
+  done
+}
+
+push_docker() {
+  local context="$1"
+  local tag="$2"
+  local version
+
+  if [[ "$CONTAINER_REGISTRY" != "ttl.sh" ]]; then
+    log "Logging in to $CONTAINER_REGISTRY" "$(line_output $LINENO)"
+    docker login \
+      --username "${REGISTRY_USER:-CONTAINER_REPO}" \
+      --password "$REGISTRY_PAT" "$CONTAINER_REGISTRY" ||
+      { err "Failed to login to $CONTAINER_REGISTRY" "$(line_output $LINENO)" >/dev/stderr; }
+  fi
+
+  log "Pushing docker images" "$(line_output $LINENO)"
+  log "CONTAINER_BUILD_CONTEXTS: $CONTAINER_BUILD_CONTEXTS" "$(line_output $LINENO)"
+  for context in $CONTAINER_BUILD_CONTEXTS; do
+    # if "$context"/.nopush exists, skip pushing this image
+    [[ -f "$context"/.nopush ]] && log ".nopush file found. Skipping $context" "$(line_output $LINENO)" && continue
+    version="$(cat "$context/.version" || echo latest)"
+    tags="$(create_version_aliases "$version")"
+
+    log "context: $context" "$(line_output $LINENO)"
+    log "tags: $tags" "$(line_output $LINENO)"
+    log "version: $version" "$(line_output $LINENO)"
+
+    # local oci_uri="$(build_oci_uri "$context" "$version" "$tags")"
+    local oci_uri="$(build_oci_uri "$context" "$tags")"
+    log "Building $oci_uri" "$(line_output $LINENO)"
+    # shellcheck disable=SC2086
+    docker push "$oci_uri" || { log "Failed to push $oci_uri. Continuing..." "$(line_output $LINENO)" >/dev/stderr; }
+  done
+}
+
+main() {
+  pushd "$REPO_ROOT" >/dev/null || exit 1
+  trap 'popd >/dev/null' EXIT
+
+  while [[ $# -gt 0 ]]; do
+    case $1 in
+    -h | --help)
+      show_help
+      exit 0
+      ;;
+    -V | --version)
+      echo "$THIS_VERSION"
+      exit 0
+      ;;
+    -v | --verbose)
+      VERBOSE=1
+      shift 1
+      ;;
+    --test | --ttl)
+      # push to ttl.sh registry; good for testing
+      CONTAINER_REGISTRY="ttl.sh"
+      CONTAINER_REPO=""
+      shift 1
+      ;;
+    -b | --build)
+      BUILD=1
+      shift 1
+      ;;
+    -p | --push)
+      PUSH=1
+      shift 1
+      ;;
+    # --all)
+    #   build_docker
+    #   push_docker
+    #   shift 1
+    #   ;;
+    # --list)
+    #   list_docker
+    #   shift 1
+    #   ;;
+    # --clean)
+    #   clean_docker
+    #   shift 1
+    #   ;;
+    -u | --update)
+      # get_latest_upstream_version
+      UPDATE=1
+      shift 1
+      ;;
+    # --shell)
+    #   /usr/bin/env bash -i
+    #   shift 1
+    #   ;;
+    *)
+      if [[ "$VERBOSE" == "1" ]]; then
+        set -x
+      fi
+
+      CONTAINER_BUILD_CONTEXTS="$*"
+      if [[ -z "$CONTAINER_BUILD_CONTEXTS" ]]; then
+        CONTAINER_BUILD_CONTEXTS="$(find_contexts)"
+      fi
+
+      if [[ "$UPDATE" == "1" ]]; then
+        get_latest_upstream_version
+      fi
+
+      if [[ "$BUILD" == "1" ]]; then
+        build_docker "$@"
+      fi
+
+      if [[ "$PUSH" == "1" ]]; then
+        push_docker "$@"
+      fi
+
+      if [[ -z "$BUILD" && -z "$PUSH" && -z "$UPDATE" ]]; then
+        build_docker "$@"
+        push_docker "$@"
+      fi
+      break
+      ;;
+    esac
+  done
+}
+
+if [[ "$DOINDOCKER" != "1" ]]; then
+  main "$@"
+  exit $?
+else
+  do_in_docker "$@"
+  exit $?
+fi
